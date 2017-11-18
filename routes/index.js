4c991d10f85e0486c073a5020f58ef4e2ffeb16a
Retwick Rajeevan <retwickrajeevan1@gmail.com>
2017-11-18 22:41:43 +0530

    #2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', rate: '100' });
+  res.render('shop/index', { title: 'Express', rate: '100' });
 });
 
 module.exports = router;
